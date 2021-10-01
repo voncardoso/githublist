b1a666d528929b44169534cc5faebf82f8d8c488
voncardoso <vonharrison26@gmail.com>
2021-10-01 20:11:56 -0300

    commit inicial

---
+++ src/components/GitList.tsx
@@ -0,0 +1,39 @@
+import '../styles/list.css';
+
+export function GitList(){
+    return(
+        <section className="GitLis">
+            <ul>
+                <li>
+                    <strong>Escola on</strong>
+                    <p>Descrição do repositprio</p>
+                    <a href="">
+                        Link do repositorio
+                    </a>
+                </li>
+                <li>
+                    <strong>Escola on</strong>
+                    <p>Descrição do repositprio</p>
+                    <a href="">
+                        Link do repositorio
+                    </a>
+                </li>
+                <li>
+                    <strong>Escola on</strong>
+                    <p>Descrição do repositprio</p>
+                    <a href="">
+                        Link do repositorio
+                    </a>
+                </li>
+                <li>
+                    <strong>Escola on</strong>
+                    <p>Descrição do repositprio</p>
+                    <a href="">
+                        Link do repositorio
+                    </a>
+                </li>
+            </ul>    
+        </section>
+        
+    );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import '../styles/header.css';
+
+export function Header(){
+    return(
+        <section className="Header">
+            <h1>List de Repositorios do Git Hub</h1>
+        </section>
+    );
+
+}
